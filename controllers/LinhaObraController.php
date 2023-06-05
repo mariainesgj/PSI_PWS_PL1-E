4a2289f2b254262bde67e29563a71fea79f35732
Maria Inês <92606230+mariainesgj@users.noreply.github.com>
2023-06-05 14:45:05 +0100

    Add files via upload

--- controllers/LinhaObraController.php
+++ controllers/LinhaObraController.php
@@ -6,12 +6,20 @@ class LinhaObraController extends Controller
 {
     public function index($id_folhaobra)
     {
-
+        $linhasobras = Linhaobra::All();
+        $this->renderView('linhaobra', 'index' , ['linhas' => $linhasobras]);
     }
 
     public function show($id)
     {
-
+    $linhasobras = Linhaobra::find([$id]);
+        if (is_null($linhasobras))
+        {
+            header('Location: ./router.php' . INVALID_ACCESS_ROUTE);
+        }
+        else {
+            $this->renderView('linhaobra' , 'show' , ['linhas' => $linhasobras] );
+        }
     }
 
     public function selectServico()
@@ -21,26 +29,52 @@ class LinhaObraController extends Controller
 
     public function create()
     {
-
+        $folhaobra = Folhaobra::all();
+        $servico = Servico::all();
+        $this->renderView('linhaobra', 'create',['servico'=>$servico,'folhaobra'=>$folhaobra]);
     }
 
     public function store()
     {
-
+        $linhasobra = new Linhaobra($_POST);
+        if($linhasobra->is_valid()){
+            $linhasobra->save();
+            $this->redirectToRoute('linhaobra','index');
+        } else {
+            $servico = Servico::all();
+            $this->renderView('linhaobra', 'create', ['linhas' => $linhasobra, 'servico'=> $servico]);
+        }
     }
 
     public function edit($id)
     {
-
+        $servico = Servico::all();
+        $folhaobra = Folhaobra::all();
+        $linhasobra = Linhaobra::find([$id]);
+        if (is_null($linhasobra)) {
+            //TODO redirect to standard error page
+            header('Location: ./router.php?' . INVALID_ACCESS_ROUTE);
+        } else {
+            $this->renderView('linhaobra', 'edit', ['linhas' => $linhasobra,'servico'=> $servico,'folhaobra'=> $folhaobra]);
+        }
     }
 
     public function update($id)
     {
-
+        $linhasobra = Linhaobra::find([$id]);
+        $linhasobra->update_attributes($_POST);
+        if($linhasobra->is_valid()){
+            $linhasobra->save();
+            $this->redirectToRoute('linhaobra', 'index');
+        } else {
+            $this->renderView('linhaobra', 'edit', ['linhas' => $linhasobra]);
+        }
     }
 
     public function delete($id)
     {
-
+        $linhasobra = linhaobra::find($id);
+        $linhasobra->delete();
+        $this->redirectToRoute('linhaobra','index');
     }
 }
